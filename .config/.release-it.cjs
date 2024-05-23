b73361a4d1884588864689e08d4d242f59381863
Gábor Torma <gabor@torma.dev>
2024-05-23 13:53:25 +0200

    Use conventional-changelog

---
+++ .config/.release-it.cjs
@@ -0,0 +1,42 @@
+// eslint-disable-next-line no-template-curly-in-string
+const version = '${version}'
+// eslint-disable-next-line node/prefer-global/process
+const packageName = process.env.npm_package_name
+const scope = packageName.split('/')[1]
+
+module.exports = {
+  plugins: {
+    '@release-it/conventional-changelog': {
+      path: '.',
+      infile: 'CHANGELOG.md',
+      preset: 'conventionalcommits',
+      gitRawCommitsOpts: {
+        path: '.',
+      },
+    },
+  },
+  git: {
+    push: true,
+    tagName: `${packageName}-v${version}`,
+    commitsPath: '.',
+    commitMessage: `feat(${scope}): released version v${version} [no ci]`,
+    requireCommits: true,
+    requireCommitsFail: false,
+
+    requireCleanWorkingDir: false,
+  },
+  npm: {
+    publish: false,
+    versionArgs: ['--workspaces false'],
+  },
+  github: {
+    release: true,
+    releaseName: `${packageName}-v${version}`,
+  },
+  /* hooks: {
+    'before:git:release': [
+      'mvm-update',
+      'git add --all',
+    ],
+  }, */
+}
