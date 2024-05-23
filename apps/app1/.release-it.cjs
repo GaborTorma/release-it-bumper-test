a2d426fcd37fba65bc7e73096865b7f9891b4353
Gábor Torma <gabor@torma.dev>
2024-05-23 14:32:38 +0200

    feat(app1): add deps and bumper

--- apps/app1/.release-it.cjs
+++ apps/app1/.release-it.cjs
@@ -1,1 +1,14 @@
-module.exports = require('../../.config/.release-it.cjs')
+module.exports = {
+  plugins: {
+    '@release-it/bumper': {
+      out: {
+        file: 'package.json',
+        path: [
+          'dependencies.package1',
+          'dependencies.package2',
+        ],
+      },
+    },
+  },
+  ...require('../../.config/.release-it.cjs'),
+}
